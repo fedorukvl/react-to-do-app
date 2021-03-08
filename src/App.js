6c18f62281ac91cc24030429a9be79830eff8235
Vasiliy Fedoruk <fedorukvl27@gmail.com>
2021-03-08 17:48:56 +0300

    В компонент App добавлены стили и начальный стейт

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+  const [toDoList, setToDoList] = useState([]);
   return (
     <div className="app">
-      <header className="app__header"></header>
+      <header className="app__header">
+        <span className="app__header-title">DO YOUR TODO</span>
+      </header>
     </div>
   );
 }
