22eaed1e6f0fab8a83a7cade05ed94656a50221e
Vasiliy Fedoruk <fedorukvl27@gmail.com>
2021-03-11 19:19:17 +0300

    Добавлена функция обработки введенных данных

--- src/App.js
+++ src/App.js
@@ -5,13 +5,20 @@ import './App.css';
 function App() {
   const [todoList, setTodoList] = useState([]);
 
+  const addTodoItem = (todo)=>{
+    setTodoList(prev => prev.concat([{
+      id: todoList.length+1,
+      ...todo,
+    }]))
+  }
+
   return (
     <div className="app">
       <header className="app__header">
         <span className="app__header-title">DO YOUR TODO</span>
       </header>
       <main>
-      <AddTodo />
+      <AddTodo addTodoItem={addTodoItem}/>
       {
         todoList.length ? <TodoList todoList={todoList}/> : <h2 className="app__warning">Пока нет дел</h2>
       }
