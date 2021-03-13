8917ed7e352112bc52a36ca787d312e9a306a8a0
Vasiliy Fedoruk <fedorukvl27@gmail.com>
2021-03-13 15:13:50 +0300

    Добавлена возможность отметки todo как выполненного

--- src/Components/Todo-item/TodoItem.jsx
+++ src/Components/Todo-item/TodoItem.jsx
@@ -1,8 +1,14 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import PropTypes from "prop-types";
 import "./TodoItem.css";
 
 function TodoItem({ todoItem, index, removeTodo }) {
+  const [isComplited, setCompleted] = useState(false);
+
+  const handleChange = () => {
+    setCompleted((prev) => !prev);
+  };
+
   const removeTodoItem = () => {
     removeTodo(index);
   };
@@ -11,11 +17,25 @@ function TodoItem({ todoItem, index, removeTodo }) {
     <Fragment>
       <h2 className="toDo-list__item-title">{todoItem.title}</h2>
       <br />
-      <div className="toDo-list__item-information">
+      <div
+        className={
+          isComplited
+            ? "toDo-list__item-information_complited"
+            : "toDo-list__item-information"
+        }
+      >
         <p className="toDo-list__item-description">{todoItem.description}</p>
-        <button className="toDo-list__delete-button" onClick={removeTodoItem}>
-          x
-        </button>
+        <div className="toDo-list__switchers">
+          <input
+            type="checkbox"
+            name="isComplited"
+            className="toDo-list__item-status"
+            onChange={handleChange}
+          />
+          <button className="toDo-list__delete-button" onClick={removeTodoItem}>
+            x
+          </button>
+        </div>
       </div>
     </Fragment>
   );
